20f1b26097ed04672825e4c53fbe0cc127e030a9
Brett Liddell <128128145+BrettLiddell@users.noreply.github.com>
2023-09-13 11:41:39 +1000

    Uploaded required files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+# Directory containing setup.py
+setup_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Path to requirements.txt
+requirements_path = os.path.join(setup_dir, "requirements.txt")
+
+REQUIREMENTS = [i.strip() for i in open(requirements_path).readlines()]
+
+setup(
+    name='meth_by_amplicon',
+    version='0.1',
+    description='CLI tool for plotting targeted bisulfite sequencing',
+    author='Brett Liddell, Olga Kondrashova', 
+    author_email='brett.liddell@qimrberghofer.edu.au, olga.kondrashova@qimrberghofer.edu.au',
+    packages=find_packages(where='src'),
+    data_files=[('bin', ['src/meth_by_amplicon/flash'])],
+    package_dir={'': 'src'},
+    install_requires=REQUIREMENTS,
+    entry_points={
+        'console_scripts': 'meth_by_amplicon = meth_by_amplicon.main:main'
+    },
+)
